6f07e4286e1a8fa41c1be24c7c193f1e33578db8
Craig Cook <boycook@me.com>
2012-11-01 12:07:20 +0000

    Added int test for signup and login

--- test/spec/ServiceSpec.js
+++ test/spec/ServiceSpec.js
@@ -3,8 +3,8 @@ var request = require('request');
 
 describe('RestService', function () {
 
-    var url = 'http://localhost:3003'; 
-    
+    var url = 'http://localhost:3003';
+
     beforeEach(function (done) {
         require('./testdata').createTestData(require('fakeredis').createClient('testdb'), done);
     });
@@ -14,6 +14,10 @@ describe('RestService', function () {
     };
     var expectedChallenge = {
         BoyCook:'null', Craig:'null', challengee:'Craig', challenger:'BoyCook', key:'BoyCook:Craig:1', winner:'null'
+    };
+
+    var newUserHulk = {
+        "username":"Hulk", "firstName":"Bruce", "lastName":"Banner", "password":"password", "email":"hulk@me.com"
     };
 
     it("should return all users", function (done) {
@@ -51,7 +55,6 @@ describe('RestService', function () {
         });
     });
 
-
     it("should return a given challenge", function (done) {
         request(url + "/challenge/BoyCook:Craig:1", function (error, response, body) {
             body = JSON.parse(body);
@@ -60,16 +63,71 @@ describe('RestService', function () {
         });
     });
 
-//    it("should return the logged in users session", function (done) {
-//        request(url + "/session", function (error, response, body) {
-////            body = JSON.parse(body);
-//            done();
-//        });
-//    });
+    it("should allow signup", function (done) {
+        request.put({
+                url:url + '/signup',
+                headers:{'content-type':'application/json', dataType:'json'},
+                body:JSON.stringify(newUserHulk)
+            },
+            function (error, response, body) {
+                body = JSON.parse(body);
+                expect(response.statusCode).toEqual(201);
+                expect(body).toEqual(newUserHulk);
+                done();
+            });
+    });
+
+    it("should not allow duplicate users to signup", function (done) {
+        request.put({
+                url:url + '/signup',
+                headers:{'content-type':'application/json', dataType:'json'},
+                body:JSON.stringify(newUserHulk)
+            },
+            function (error, response, body) {
+                body = JSON.parse(body);
+                expect(response.statusCode).toEqual(409);
+                expect(body).toEqual({message:'User with name [Hulk] already exists'});
+                done();
+            });
+    });
+
+    it("should allow login and return session object", function (done) {
+        request.post(url + '/login?username=BoyCook&password=password',
+            function (error, response, body) {
+                body = JSON.parse(body);
+                expect(response.statusCode).toEqual(200);
+                expect(body).toEqual({user:expectedBoyCook});
+                done();
+            });
+    });
+
+    it("should return the logged in users session", function (done) {
+        request(url + "/session", function (error, response, body) {
+            body = JSON.parse(body);
+            expect(response.statusCode).toEqual(200);
+            expect(body).toEqual({user:expectedBoyCook});
+            done();
+        });
+    });
+
+    it("should fail login for invalid user", function (done) {
+        request.post(url + '/login?username=INVALID&password=password',
+            function (error, response, body) {
+                expect(response.statusCode).toEqual(401);
+                done();
+            });
+    });
+
+    it("should fail login for invalid password", function (done) {
+        request.post(url + '/login?username=BoyCook&password=invalid',
+            function (error, response, body) {
+                expect(response.statusCode).toEqual(401);
+                done();
+            });
+    });
 
 
 //    app.put('/user/:name/challenges/:challengee', game.challenge);
 //    app.put('/challenge/:key/:user/:move', game.makeMove);
-//    app.put('/signup', session.signUp);
 //    app.post('/login', passport.authenticate('local'), session.authenticateSuccess);
 });
