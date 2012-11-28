1b751ebbcc575279e785ff143a5bda4f4fc4d124
Craig Cook <boycook@me.com>
2012-11-28 18:33:56 +0000

    Added ulimit

--- startup.sh
+++ startup.sh
@@ -19,6 +19,7 @@ ACTION=$1
 if [ "$ACTION" == "START" ] || [ "$ACTION" == "start" ] 
 then
 	echo 'Starting service...'
+	#ulimit -n 5000
 	#export NODE_ENV=production
 	nohup node server.js | tee  server.log &
 elif [ "$ACTION" == "STOP" ] || [ "$ACTION" == "stop" ]
@@ -32,3 +33,6 @@ then
 else
 	echo "Unknown action ($ACTION) - Doing nothing"
 fi
+
+
+
