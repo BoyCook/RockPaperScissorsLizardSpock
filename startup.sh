e7c5503100ed203915651bb19b1ba4eea351d736
Craig Cook <boycook@me.com>
2012-12-11 21:26:12 +0000

    Added DB to startup

--- startup.sh
+++ startup.sh
@@ -19,13 +19,17 @@ ACTION=$1
 if [ "$ACTION" == "START" ] || [ "$ACTION" == "start" ] 
 then
 	echo 'Starting service...'
-	#ulimit -n 5000
-	#export NODE_ENV=production
+	ulimit -n 5000
+	export NODE_ENV=production
 	nohup node server.js | tee  server.log &
 elif [ "$ACTION" == "STOP" ] || [ "$ACTION" == "stop" ]
 then
     echo 'Stopping service...'
     pkill node
+elif [ "$ACTION" == "DB" ] || [ "$ACTION" == "db" ]
+then
+    echo 'Starting DB...'
+    nohup $REDIS_HOME/src/redis-server | tee redis.log &
 elif [ "$ACTION" == "TAIL" ] || [ "$ACTION" == "tail" ]
 then
     echo 'Tailing service...'
@@ -33,6 +37,3 @@ then
 else
 	echo "Unknown action ($ACTION) - Doing nothing"
 fi
-
-
-
