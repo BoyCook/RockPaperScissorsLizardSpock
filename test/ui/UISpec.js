19b5dff28a568aa80e8dfebd5778f43799bacb55
Craig Cook <boycook@me.com>
2013-03-09 18:05:31 +0000

    Started login test - troubles will form values

--- test/ui/UISpec.js
+++ test/ui/UISpec.js
@@ -1,6 +1,6 @@
 var casper = require('casper').create();
 
-casper.start('http://localhost:3000/', function() {
+casper.start('http://localhost:3000/', function () {
     this.test.assertTitle('Rock Paper Scissors Lizard Spock [beta]', 'RPSLP homepage title is the one expected');
     this.test.assertExists('.rules', 'rules module is found');
     this.test.assertExists('.game-remote', 'game-remote module is found');
@@ -12,7 +12,19 @@ casper.start('http://localhost:3000/', function() {
     this.test.assertExists('.play-flash', 'play-flash module is found');
 });
 
-casper.run(function() {
+//Do login
+//casper.then(function () {
+//    this.click('.show-login');
+////    'form[name="login-form"]'
+//    this.fill('form#login-form', {
+//            username: 'BoyCook',
+//            password: 'password'
+//        }, false);
+//    this.test.assertField('username', 'BoyCook');
+//    this.test.assertField('password', 'password');
+//});
+
+casper.run(function () {
     this.test.renderResults(true, 0, 'reports/TEST-UISpec.xml');
     this.test.done(5); // checks that 5 assertions have been executed
     this.test.renderResults(true);
