2681f3ca07ade0f0433d4065fbbc673128c216fd
Craig Cook <boycook@me.com>
2013-03-16 17:43:11 +0000

    Doing test serve on test port

--- serve.js
+++ serve.js
@@ -1,5 +1,5 @@
 var db = require('fakeredis').createClient('testdb');
 
-var server = require('./lib/server.js').startUp({port: 3000}, function() {
+var server = require('./lib/server.js').startUp({port: 3003}, function() {
     require('./test/spec/testdata').createTestData(db);
 });
