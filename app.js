5d86d4cee00732122ed6efee97cf6be8d1792c88
Craig Cook <craig.cook@code.red>
2018-03-26 18:37:16 +0100

    Added in APM

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
+var apm = require('elastic-apm-node').start({
+    // Set required service name (allowed characters: a-z, A-Z, 0-9, -, _, and space)
+    serviceName: 'Rock Paper Scissors Lizard Spock',
+    // Use if APM Server requires a token
+    secretToken: '',
+    // Set custom APM Server URL (default: http://localhost:8200)
+    serverUrl: 'http://localhost:8200'
+})
 
 require('./lib/server.js').startUp({ port: process.env.PORT || 3000 });
